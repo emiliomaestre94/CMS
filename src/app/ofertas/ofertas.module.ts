cff9017edd9f114e85537b2a31ba5d26c000868f
Jose Fco Moreno Fenandez <josefcomoreno85@gmail.com>
2017-03-07 20:19:26 +0100

    Ajustes de tamaños de tablas, datapicker y casi PerfilAdmin

--- src/app/ofertas/ofertas.module.ts
+++ src/app/ofertas/ofertas.module.ts
@@ -1,3 +1,4 @@
+
 import { OfertasRoutingModule } from './ofertas-routing.module';
 import { OfertasComponent } from './ofertas.component';
 import { BuscadorOfertasPipe } from './buscadorofertas.pipe';
@@ -12,6 +13,7 @@ import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { DropdownModule } from 'ng2-bootstrap/dropdown';
 import { CommonModule } from '@angular/common'; //esto es para que funcione el ngif, nfgor etc
+import { DatePickerModule } from 'ng2-datepicker';
 
 import { AUTH_PROVIDERS } from 'angular2-jwt';
 import { provideAuth } from 'angular2-jwt';
@@ -25,6 +27,7 @@ BuscadorOfertasPipe
       FormsModule,
       ChartsModule,
       DropdownModule,
+      DatePickerModule, 
       ModalModule.forRoot()
     ],
 
